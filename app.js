d4c09cbc77439af4d642213b18e801f9e3d8963f
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2017-02-03 10:31:24 +0100

    back to basics

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
-'use strict';
+// 'use strict';
 
-var app = require('express')();
-var bodyParser = require('body-parser');
+// var app = require('express')();
+// var bodyParser = require('body-parser');
 
-var getRoutes = require('./routes/get.js');
-var postRoutes = require('./routes/post.js');
+// var getRoutes = require('./routes/get.js');
+// var postRoutes = require('./routes/post.js');
 
-var port = process.argv[2] || process.env.port || 9100;
+// var port = process.argv[2] || process.env.port || 9100;
 
-// CORS Headers
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// // CORS Headers
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use('/', getRoutes);
-//app.use('/', postRoutes);
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({ extended: true }));
+// app.use('/', getRoutes);
+// app.use('/', postRoutes);
 
-app.listen(port, () => {
-  console.log('App listening on port ' + port);
-});
+// app.listen(port, () => {
+//   console.log('App listening on port ' + port);
+// });
 
-// var http = require('http')
-//  var port = process.env.port || 1337;
-//  http.createServer(function(req, res) {
-//    res.writeHead(200, { 'Content-Type': 'text/plain' });
-//    res.end('Hello World\n');
-//  }).listen(port);
+var http = require('http')
+ var port = process.env.port || 1337;
+ http.createServer(function(req, res) {
+   res.writeHead(200, { 'Content-Type': 'text/plain' });
+   res.end('Hello World\n');
+ }).listen(port);
