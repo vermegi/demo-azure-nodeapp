cc148ec0b5cbb31f869959a4582acef70132cb05
Gitte Vermeiren <gitte.vermeiren@gmail.com>
2017-02-03 09:58:07 +0100

    fixed error

--- app.js
+++ app.js
@@ -25,7 +25,7 @@
 // });
 
 var http = require('http')
- var port = 80;
+ var port = 8080;
  http.createServer(function(req, res) {
    res.writeHead(200, { 'Content-Type': 'text/plain' });
    res.end('Hello World\n');
